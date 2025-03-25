4c29620854529ed335069507f63e0c5fad8f07d3
thomas-corteval <113097597+thomas-corteval@users.noreply.github.com>
2025-03-25 12:58:19 +0100

    Reglage des problème de BDD

--- migrations/Version20250325115730.php
+++ migrations/Version20250325115730.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250317141145 extends AbstractMigration
+final class Version20250325115730 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,10 @@ final class Version20250317141145 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE artist (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(2047) NOT NULL, picture VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE event (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, artist_id INTEGER NOT NULL, creator_id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, date DATE NOT NULL, CONSTRAINT FK_3BAE0AA7B7970CF8 FOREIGN KEY (artist_id) REFERENCES artist (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_3BAE0AA761220EA6 FOREIGN KEY (creator_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_3BAE0AA7B7970CF8 ON event (artist_id)');
+        $this->addSql('CREATE INDEX IDX_3BAE0AA761220EA6 ON event (creator_id)');
         $this->addSql('CREATE TABLE participe (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)');
         $this->addSql('CREATE TABLE participe_event (participe_id INTEGER NOT NULL, event_id INTEGER NOT NULL, PRIMARY KEY(participe_id, event_id), CONSTRAINT FK_A9BA512BA71D81B9 FOREIGN KEY (participe_id) REFERENCES participe (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_A9BA512B71F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_A9BA512BA71D81B9 ON participe_event (participe_id)');
@@ -27,13 +31,27 @@ final class Version20250317141145 extends AbstractMigration
         $this->addSql('CREATE TABLE participe_user (participe_id INTEGER NOT NULL, user_id INTEGER NOT NULL, PRIMARY KEY(participe_id, user_id), CONSTRAINT FK_4B46C185A71D81B9 FOREIGN KEY (participe_id) REFERENCES participe (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_4B46C185A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_4B46C185A71D81B9 ON participe_user (participe_id)');
         $this->addSql('CREATE INDEX IDX_4B46C185A76ED395 ON participe_user (user_id)');
+        $this->addSql('CREATE TABLE "user" (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME ON "user" (username)');
+        $this->addSql('CREATE TABLE messenger_messages (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, body CLOB NOT NULL, headers CLOB NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , available_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , delivered_at DATETIME DEFAULT NULL --(DC2Type:datetime_immutable)
+        )');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE artist');
+        $this->addSql('DROP TABLE event');
         $this->addSql('DROP TABLE participe');
         $this->addSql('DROP TABLE participe_event');
         $this->addSql('DROP TABLE participe_user');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
